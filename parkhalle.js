13fae739e359d18aeb8a653bad435fa7544edd84
Diego <diegoakowi@gmail.com>
2021-04-19 11:41:07 +0200

    update

--- parkhalle.js
+++ parkhalle.js
@@ -1,8 +1,24 @@
 class Parkhalle
 {
-    constructor() 
+    constructor(canvas) 
     {
-        let p1 = new Parkplatz(1,2);
-        p1.platzZeichnen();
+        this.canvas = canvas;
+        parkplaetze = [];
+
+        this.platzEinfuegen(20,20);
+    }
+
+    platzEinfuegen(x,y)
+    {
+        let p1 = new Parkplatz(x,y);
+        parkplaetze.push(p1);
+    }
+
+    plaetzeZeichnen()
+    {
+        for(i=0;i<5;i++)
+        {
+            parkplaetze[i].platzZeichnen(canvas);
+        }
     }
 }
