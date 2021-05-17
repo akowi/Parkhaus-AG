9d1068f98c3d847ac8825dede0d500f98a7357ab
Diego <diegoakowi@gmail.com>
2021-05-17 11:45:44 +0200

    Update

--- parkplatz.js
+++ parkplatz.js
@@ -13,17 +13,6 @@ class Parkplatz
     {
         return frei;
     }
-    
-    canvasZeichnen()
-    {
-        var canvas = document.createElement('canvas');
-        canvas.width = 2000;
-        canvas.height = 2000;
-        var body = document.getElementsByTagName("body")[0];
-        body.appendChild(canvas);
-        var ctx = canvas.getContext("2d");
-        return ctx;
-    }
 
     platzZeichnen(x,y,ctx)
     {  
