cef3192f80cdcb53cb941f40ed15462ca80c88c8
Diego <diegoakowi@gmail.com>
2021-05-17 10:19:45 +0200

    Finally works

--- parkhalle.js
+++ parkhalle.js
@@ -2,16 +2,28 @@ class Parkhalle
 {
     constructor() 
     {
-        parkplaetze = [];
+        console.log("pog");
+        this.canvas = document.getElementById("canvas");
+        var ctx = canvas.getContext('2d');
+        this.canvas = 
+        {
+            width:500,
+            height:500
+        };
 
-        this.platzEinfuegen(20,20);
-    }
+        ctx.beginPath();
+        ctx.rect(20, 20, 150, 100);
+        ctx.stroke();
+
+        
+       
+    };
 
     platzEinfuegen(x,y)
     {
         let p1 = new Parkplatz(x,y);
         parkplaetze.push(p1);
-    }
+    };
 
     plaetzeZeichnen()
     {
@@ -19,5 +31,7 @@ class Parkhalle
         {
             parkplaetze[i].platzZeichnen();
         }
-    }
+    };
 }
+
+let parkhalle = new Parkhalle();
--- parkplatz.js
+++ parkplatz.js
@@ -14,13 +14,13 @@ class Parkplatz
         return frei;
     }
     
-    platzZeichnen()
+    platzZeichnen(ctx)
     {  
-        var canvas = document.createElement("CANVAS")
-        var ctx = canvas.getContext("2d");
-        ctx.beginPath();
-        ctx.rect(20, 20, 150, 100);
-        ctx.stroke();
-        document.body.appendChild(canvas);
+        this.ctx = ctx;
+        //ctx.beginPath();
+        //ctx.rect(20, 20, 150, 100);
+        //ctx.stroke();
+        //ctx.fillStyle = "red";
+        //ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
