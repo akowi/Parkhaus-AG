f892ef91951d476808b653c56c7163e9eff14380
Diego <diegoakowi@gmail.com>
2021-05-24 10:23:39 +0200

    Update

---
+++ main.js
@@ -0,0 +1,13 @@
+function canvasZeichnen()
+{
+        var canvas = document.createElement('canvas');
+        canvas.width = 2000;
+        canvas.height = 2000;
+        var body = document.getElementsByTagName("body")[0];
+        body.appendChild(canvas);
+        var ctx = canvas.getContext("2d");
+        return ctx;
+}
+
+var parkplaetze = new Array();
+let parkhalle = new Parkhalle(20);
