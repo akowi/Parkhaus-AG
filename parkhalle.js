d5844733e5ca2d44920466d84c4404dfab7f09bc
Diego <diegoakowi@gmail.com>
2021-05-24 11:43:54 +0200

    POGGERS

--- parkhalle.js
+++ parkhalle.js
@@ -4,16 +4,17 @@ class Parkhalle
     {
         console.log("pog");
         
-        ctx = canvasZeichnen();
+        this.ctx = canvasZeichnen();
         this.plaetzeZeichnen(anzahl);
         
-    };
+    }
+
 
     platzEinfuegen(x,y,id)
     {
         let p1 = new Parkplatz(x,y,id);
         parkplaetze.push(p1);
-    };
+    }
 
     plaetzeZeichnen(anzahl)
     {
@@ -21,14 +22,17 @@ class Parkhalle
         var quotient = Math.floor(anzahl/2);
         var rest = anzahl % 2;
 
-        for(var i=0;i<(quotient + rest);i++)
+        for(var n=0;n<2;n++)
         {
-            var hoehe = 100;
-            var px = 20;
-            var yx = 50 + i*hoehe;
-            this.platzEinfuegen(px,yx,i);
-            parkplaetze[i].platzZeichnen(x,y,ctx);
+            for(var i=0;i<(quotient + rest);i++)
+            {
+                var hoehe = 100;
+                var px = 200+500*n;
+                var py = 50 + i*hoehe;
+                this.platzEinfuegen(px,py,i+n*(quotient + rest));
+                parkplaetze[i+n*(quotient + rest)].platzZeichnen(px,py,this.ctx);
+            }
         }
-    };
+    }
 }
 
--- parkplatz.js
+++ parkplatz.js
@@ -5,21 +5,35 @@ class Parkplatz
         this.x = x;
         this.y = y;
         this.id = id;
-        var frei;
+        this.frei=true;
         
     }
 
-    istFrei()
+
+    farbeBestimmen()
     {
-        return frei;
+        var farbe;
+
+        if(!this.frei)
+        {
+            farbe = "red";
+        }
+        else
+        {
+            farbe = "limegreen";
+        }
+        return farbe;
     }
 
     platzZeichnen(x,y,ctx)
     {  
         this.ctx = ctx;
-    
         ctx.beginPath();
+        console.log(this.farbeBestimmen());
+        ctx.fillStyle = this.farbeBestimmen();
+        ctx.lineWidth = 10;
         ctx.rect(x, y, 150, 100);
         ctx.stroke();
+        ctx.fill();
     }
 }
