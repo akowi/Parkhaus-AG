3e23041d28b088be0ef914aafd9bff49d1a19e6e
akowi <diegoakowi@gmail.com>
2021-06-15 19:23:13 +0200

    xd

--- db_verbindung.php
+++ db_verbindung.php
@@ -5,8 +5,7 @@ function verbindungHerstellen()
  $dbhost = "sql11.freemysqlhosting.net";
  $dbuser = "sql11419329";
  $dbpass = "Ic8EEYcppA";
- $db = "example";
- $verb = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Verbindung fehlgeschlagen: %s\n". $verb -> error);
+ $verb = new mysqli($dbhost, $dbuser, $dbpass) or die("Verbindung fehlgeschlagen: %s\n". $verb -> connect_error);
  
  return $verb;
  }
